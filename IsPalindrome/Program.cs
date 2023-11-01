b34d1c994fe49d15432e73012ac19fe952e7c7aa
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-01 14:44:39 +0300

    Добавьте файлы проекта.

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(new Solution().IsPalindrome(121));
+Console.WriteLine(new Solution().IsPalindrome(-121));
+Console.WriteLine(new Solution().IsPalindrome(10));
+Console.WriteLine(new Solution().IsPalindrome(11));
+Console.ReadKey();
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        bool result = true;
+        var x_str = x.ToString();
+        int str_len = x_str.Length;
+        int part_len = str_len / 2;
+        for(int i = 0; i < part_len; i++)
+        {
+            if (x_str[i] != x_str[str_len - i - 1])
+            {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(string.Join(" ", new Solution().TwoSum(new int[] { 1, 2, 3 }, 6)));
+Console.WriteLine(string.Join(" ", new Solution().TwoSum(new int[] { 3, 3 }, 6)));
+Console.WriteLine(string.Join(" ", new Solution().TwoSum(new int[] { 1, 4, 6 }, 10)));
+Console.ReadKey();
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        int len = nums.Length;
+        int[] result = new int[2];
+        for (int i = 0; i < len; i++)
+        {
+            int to_find = target - nums[i];
+            if (dic.ContainsKey(to_find))
+            {
+                result[0] = dic[to_find];
+                result[1] = i;
+                break;
+            }
+            else if (!dic.ContainsKey(nums[i]))
+            {
+                dic.Add(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
