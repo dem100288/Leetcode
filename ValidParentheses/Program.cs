b931cdbe44d0aff2a2b8f72518210772292b4f05
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-01 19:59:29 +0300

    Valid Parentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine(new Solution().IsValid("()"));
+Console.WriteLine(new Solution().IsValid("()[]{}"));
+Console.WriteLine(new Solution().IsValid("[{}]"));
+
+Console.WriteLine(new Solution().IsValid("[{[}{]}]"));
+Console.WriteLine(new Solution().IsValid("({([]}{()}[])[{}])"));
+Console.WriteLine(new Solution().IsValid("(({[]}{([]})[])[{}])"));
+Console.WriteLine(new Solution().IsValid("((({[]}{)})))"));
+Console.WriteLine(new Solution().IsValid("[({}])"));
+Console.WriteLine(new Solution().IsValid("(]"));
+Console.ReadKey();
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> a = new();
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '(':
+                    a.Push(')');
+                    break;
+                case '{':
+                    a.Push('}');
+                    break;
+                case '[':
+                    a.Push(']');
+                    break;
+                case ']':
+                case ')':
+                case '}':
+                    if (a.Count == 0) return false;
+                    var ac = a.Pop();
+                    if (ac != c) return false;
+                    break;
+            }
+        }
+
+        return a.Count == 0;
+    }
+}
