e08107f0a1cee1eb9f33a4a132d4b28aa77002f9
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-03 11:08:30 +0300

    FindIndexFirstOccurrence

---
+++ FindIndexFirstOccurrence/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(new Solution().StrStr("sadbutsad", "sad"));
+Console.WriteLine(new Solution().StrStr("leetcode", "leeto"));
+Console.ReadKey();
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
