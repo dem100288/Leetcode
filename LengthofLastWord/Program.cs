a5fe25eae8b2b5b8be782beecf6062d74f05f694
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-03 13:14:50 +0300

    Length of Last Word

---
+++ LengthofLastWord/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(new Solution().LengthOfLastWord("Hello World"));
+Console.WriteLine(new Solution().LengthOfLastWord("   fly me   to   the moon  "));
+Console.WriteLine(new Solution().LengthOfLastWord("luffy is still joyboy"));
+
+Console.ReadKey();
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        int count = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ') count++;
+            if (s[i] == ' ' && count > 0) break;
+        }
+        return count;
+    }
+}
