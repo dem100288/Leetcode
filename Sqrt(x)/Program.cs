44a4f7ca60353bb5c4e0202ce308a5d90f3dd1b4
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-08 15:41:30 +0300

    Sqrt(x)

---
+++ Sqrt(x)/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(new Solution().MySqrt(4));
+Console.WriteLine(new Solution().MySqrt(8));
+Console.WriteLine(new Solution().MySqrt(2147395600));
+
+Console.ReadKey();
+
+public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if (x <= 1) return x;
+        double xk = 2;
+        double xk1 = 0.5 * (xk + x / xk);
+        while (Math.Abs(xk1 - xk) >= 1)
+        {
+            xk = xk1;
+            xk1 = 0.5 * (xk + x / xk);
+        }
+        return (int)xk1;
+    }
+}
