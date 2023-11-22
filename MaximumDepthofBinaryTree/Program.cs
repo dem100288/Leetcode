e7f5b5d9b6e368978ae373de7f4bbf0c4f5d653d
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-22 10:38:37 +0300

    Maximum Depth of Binary Tree

---
+++ MaximumDepthofBinaryTree/Program.cs
@@ -0,0 +1,35 @@
+﻿var t3 = new TreeNode(3);
+var t2 = new TreeNode(2, t3, null);
+var t1 = new TreeNode(1, null, t2);
+
+//new Solution().IsSameTree(t1);
+
+
+Console.ReadKey();
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public int RecNode(TreeNode node, int depth)
+    {
+        if (node == null) return depth;
+        depth++;
+        return Math.Max(RecNode(node.left, depth), RecNode(node.right, depth));
+    }
+    public int MaxDepth(TreeNode root)
+    {
+        return RecNode(root, 0);
+    }
+}
