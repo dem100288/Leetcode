d75ff2fd5eee5293858e509a17fef8c69865f227
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-28 12:03:26 +0300

    PascalsTriangle2

---
+++ PascalsTriangle2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(new Solution().GetRow(0));
+Console.WriteLine(new Solution().GetRow(1));
+Console.WriteLine(new Solution().GetRow(2));
+Console.WriteLine(new Solution().GetRow(3));
+Console.WriteLine(new Solution().GetRow(4));
+Console.WriteLine(new Solution().GetRow(5));
+
+
+Console.ReadKey();
+
+public class Solution
+{
+    public IList<int> GetRow(int rowIndex)
+    {
+        if (rowIndex == 0)
+        {
+            return new List<int>(1) { 1 };
+        }
+        if (rowIndex == 1)
+        {
+            return new List<int>(2) { 1, 1 };
+        }
+        var prevLine = new List<int>(2) { 1, 1 };
+        for (int i = 2; i <= rowIndex; ++i)
+        {
+            var newLine = new List<int>() { 1 };
+            for (int z = 1; z < i; ++z)
+            {
+                newLine.Add(prevLine[z - 1] + prevLine[z]);
+            }
+            newLine.Add(1);
+            prevLine = newLine;
+        }
+
+        return prevLine;
+    }
+}
