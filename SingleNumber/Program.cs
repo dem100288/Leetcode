341db2f3054233037ded3b5fe4afb3515f08b178
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-11-29 15:04:40 +0300

    Single Number

---
+++ SingleNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿//Single Number
+Console.WriteLine(new Solution().SingleNumber(new int[] { 2, 2, 1 }));
+Console.WriteLine(new Solution().SingleNumber(new int[] { 4, 1, 2, 1, 2 }));
+Console.WriteLine(new Solution().SingleNumber(new int[] { 1 }));
+
+Console.ReadKey();
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int res = 0;
+        foreach (int num in nums)
+        {
+            res ^= num;
+        }
+        return res;
+    }
+}
