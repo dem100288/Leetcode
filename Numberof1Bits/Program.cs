421ea3494a0480fe33413e0d6318e2c3082eb7c8
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-12-04 15:09:49 +0300

    Number of 1 Bits

---
+++ Numberof1Bits/Program.cs
@@ -0,0 +1,16 @@
+﻿//Number of 1 Bits
+
+Console.WriteLine(new Solution().HammingWeight(0b00000000000000000000000000001011)); // 3
+Console.WriteLine(new Solution().HammingWeight(0b00000000000000000000000010000000)); // 1
+Console.WriteLine(new Solution().HammingWeight(0b11111111111111111111111111111101)); // 31
+Console.WriteLine(new Solution().HammingWeight(0b00000000000000000000000000000000)); // 0
+
+Console.ReadKey();
+
+public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        return Convert.ToString(n, toBase: 2).Count(x => x == '1');
+    }
+}
