f33241505832eb00c2b0c9154aa2335399cd4bae
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-12-06 17:04:38 +0300

    Happy Number

---
+++ HappyNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿//Happy Number
+
+Console.WriteLine(new Solution().IsHappy(19)); // true
+Console.WriteLine(new Solution().IsHappy(2)); // false
+Console.WriteLine(new Solution().IsHappy(1)); // true
+
+Console.ReadKey();
+
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        List<int> list_nums = new List<int>();
+        while (!list_nums.Contains(n) && n != 1)
+        {
+            list_nums.Add(n);
+            n = n.ToString().Select(x => (int)Math.Pow(Convert.ToUInt32(x.ToString()), 2)).Sum();
+        }
+        return n == 1;
+    }
+}
