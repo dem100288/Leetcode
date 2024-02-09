5ddc76d40cd548c93d1ea2ae7e4cf279db3db68a
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-02-09 15:00:15 +0300

    ContainsDuplicate

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(new Solution().ContainsDuplicate(new int[] { 1, 2, 3, 1 }));
+Console.WriteLine(new Solution().ContainsDuplicate(new int[] { 1, 2, 3, 4 }));
+Console.WriteLine(new Solution().ContainsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
+
+
+Console.ReadKey();
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        return new HashSet<int>(nums).Count < nums.Length;
+    }
+}
