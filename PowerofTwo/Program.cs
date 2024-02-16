ba1626dcc532d86182b8348c0f780dfa0ef1ce5a
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-02-16 11:23:46 +0300

    231. Power of Two

---
+++ PowerofTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(new Solution().IsPowerOfTwo(1));
+Console.WriteLine(new Solution().IsPowerOfTwo(16));
+Console.WriteLine(new Solution().IsPowerOfTwo(3));
+Console.WriteLine(new Solution().IsPowerOfTwo(0));
+Console.WriteLine(new Solution().IsPowerOfTwo(-2147483648));
+Console.WriteLine(new Solution().IsPowerOfTwo(-16));
+
+Console.ReadKey();
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        return n <= 0 ? false : (n & n - 1) == 0;
+    }
+}
