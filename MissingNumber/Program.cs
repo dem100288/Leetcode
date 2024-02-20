b1dca6e6cd626c29ebf4b30c38ff02556c66241f
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-02-20 09:50:51 +0300

    268. Missing Number

---
+++ MissingNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(new Solution().MissingNumber(new int[] { 3, 0, 1 })); // 2
+Console.WriteLine(new Solution().MissingNumber(new int[] { 0, 1 })); // 2
+Console.WriteLine(new Solution().MissingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 })); // 8
+
+Console.ReadKey();
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        return ((nums.Length * (nums.Length + 1)) / 2) - nums.Sum();
+    }
+}
