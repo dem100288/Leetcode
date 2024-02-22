b5e94250f4a1cbde53a194b2f56aa6e86861c928
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-02-22 13:40:07 +0300

    242. Valid Anagram

---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(new Solution().IsAnagram("anagram", "nagaram")); // true
+Console.WriteLine(new Solution().IsAnagram("rat", "car")); // false
+Console.WriteLine(new Solution().IsAnagram("abb", "aab")); // false
+
+
+Console.ReadKey();
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        return s.Length == t.Length ? (new HashSet<char>(s).All(x => s.Count(y => y == x) == t.Count(y => y == x))) : false;
+    }
+}
