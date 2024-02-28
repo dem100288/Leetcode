364e46a3ce19af21056d589960ec44db1615a517
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-02-28 15:14:40 +0300

    290. Word Pattern

---
+++ WordPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(new Solution().WordPattern("abba", "dog cat cat dog")); // true
+Console.WriteLine(new Solution().WordPattern("abba", "dog cat cat fish")); // false
+Console.WriteLine(new Solution().WordPattern("aaaa", "dog cat cat dog")); // false
+Console.WriteLine(new Solution().WordPattern("abba", "dog dog dog dog")); // false
+
+Console.ReadKey();
+
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        Dictionary<char, string> dic = new Dictionary<char, string>();
+        var words = s.Split(' ');
+        if (pattern.Length != words.Length) return false;
+        for (int i = 0; i < words.Length; ++i)
+        {
+            if (dic.TryGetValue(pattern[i], out string? word))
+            {
+                if (words[i] != word) return false;
+            }
+            else
+            {
+                if (dic.Values.Contains(words[i])) return false;
+                dic.Add(pattern[i], words[i]);
+            }
+        }
+        return true;
+    }
+}
