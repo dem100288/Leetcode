fe18e9f4237239022c696fef610c29fc214c5831
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-02-28 13:05:58 +0300

    283. Move Zeroes

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,28 @@
+﻿var nums = new int[] { 0, 1, 0, 3, 12 };
+new Solution().MoveZeroes(nums); // 1,3,12,0,0
+nums = new int[] { 0 };
+new Solution().MoveZeroes(nums); // 0
+Console.WriteLine(nums);
+
+Console.ReadKey();
+
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int last_num = 0;
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            if (nums[i] != 0)
+            {
+                if (last_num != i)
+                {
+                    nums[last_num] = nums[i];
+                    nums[i] = 0;
+                }
+                last_num++;
+            }
+        }
+    }
+}
