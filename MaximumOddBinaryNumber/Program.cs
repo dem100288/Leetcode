92acf567506df0b9b03aaba3e69e8e5bc54c8829
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-03-01 10:38:10 +0300

    2864. Maximum Odd Binary Number

---
+++ MaximumOddBinaryNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+Console.WriteLine(new Solution().MaximumOddBinaryNumber("010")); // "001"
+Console.WriteLine(new Solution().MaximumOddBinaryNumber("0101")); // "1001"
+Console.WriteLine(new Solution().MaximumOddBinaryNumber("1")); // "1"
+
+Console.ReadKey();
+
+public class Solution
+{
+    public string MaximumOddBinaryNumber(string s)
+    {
+        var count_1 = s.Count(x => x == '1');
+        return new StringBuilder().Append('1', count_1 - 1).Append('0', s.Length - count_1).Append("1").ToString();
+    }
+}
