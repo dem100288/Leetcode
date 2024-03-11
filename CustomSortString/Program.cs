3e5ef537b3d7ceb02bf77a314298f1cf8667d024
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2024-03-11 09:37:13 +0300

    791. Custom Sort String

---
+++ CustomSortString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+Console.WriteLine(new Solution().CustomSortString("cba", "abcd")); // cbad
+Console.WriteLine(new Solution().CustomSortString("bcafg", "abcd")); // bcad
+
+Console.ReadKey();
+
+public class Solution
+{
+    public string CustomSortString(string order, string s)
+    {
+        Dictionary<char, int> letters = new Dictionary<char, int>(order.Select(x => new KeyValuePair<char, int>(x, 0)));
+        List<char> tail = new List<char>();
+        foreach (var letter in s)
+        {
+            if (letters.ContainsKey(letter))
+            {
+                letters[letter] += 1;
+            }
+            else
+            {
+                tail.Add(letter);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        foreach (var letter in letters)
+        {
+            sb.Append(letter.Key, letter.Value);
+        }
+        sb.AppendJoin("", tail);
+        return sb.ToString();
+    }
+}
