04e58136a6e9bc8e6c71453f6b08225a7e583bdc
DMITRY ZAKHAROV <dem100288@gmail.com>
2025-10-14 11:09:49 +0300

    AdjacentIncreasingSubarraysDetectionI

---
+++ AdjacentIncreasingSubarraysDetectionI/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AdjacentIncreasingSubarraysDetectionI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasIncreasingSubarrays([2, 5, 7, 8, 9, 2, 3, 4, 3, 1], 3));
+            Console.WriteLine(HasIncreasingSubarrays([1, 2, 3, 4, 4, 4, 4, 5, 6, 7], 5));
+            Console.WriteLine(HasIncreasingSubarrays([5, 8, -2, -1], 2));
+            Console.WriteLine(HasIncreasingSubarrays([8, 9, 13, -10, -9, -3], 3));
+        }
+
+        public static bool HasIncreasingSubarrays(IList<int> nums, int k)
+        {
+            if (k == 1)
+            {
+                return true;
+            }
+
+            var iMax = nums.Count - k;
+
+            int lastA = int.MaxValue;
+            int lastB = int.MaxValue;
+            int countA = 0;
+            int countB = 0;
+            for (int i = 0; i < iMax; i++)
+            {
+                if (lastA >= nums[i] || lastB >= nums[i + k])
+                {
+                    countA = 0;
+                    countB = 0;
+                }
+
+                lastA = nums[i];
+                lastB = nums[i + k];
+                countA++;
+                countB++;
+
+                if (countA == k && countB == k)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SpecialArrayWithXElementsGreaterThanorEqualX/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine(new Solution().SpecialArray([3, 5])); // 2
+Console.WriteLine(new Solution().SpecialArray([0, 0])); // -1
+Console.WriteLine(new Solution().SpecialArray([0, 4, 3, 0, 4])); // 3
+Console.ReadKey();
+
+public class Solution {
+    public int SpecialArray(int[] nums)
+    {
+        int min = nums[0];
+        int num = 0;
+        bool flag = false;
+        for (int i = 0; i < nums.Length;)
+        {
+            if (nums[i] >= num)
+            {
+                num++;
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                }
+
+                flag = num <= min;
+            }
+            if (num > min)
+            {
+                min = num;
+                i = 0;
+                num = 0;
+                flag = false;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return num <= min && flag ? num : -1;
+    }
+}
